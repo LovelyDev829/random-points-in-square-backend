fd634d5e6953a477f2aa17e61c55a7d57c5c2c96
LovelyDev829 <lovelydeveloper829@gmail.com>
2022-10-28 10:46:24 +0900

    username logs updated

--- routes/user.route.js
+++ routes/user.route.js
@@ -7,26 +7,36 @@ let userSchema = require('../models/User.js')
 
 // CREATE
 router.route('/create-user').post((req, res, next) => {
-  const email = req.body.email
+  const { email, userName } = req.body
   console.log("create-user", req.body)
   userSchema.find({ email: email }, (error, data) => {
     if (error) {
-      res.json({ success: false })
+      res.json({ success: false, message: "There was an error..." })
     }
     else if (data[0]) {
-      console.log("Already exist the user")
-      res.json({ success: false })
+      console.log("Already existing email address...")
+      res.json({ success: false, message: "Already existing email address..." })
     } else {
-      var today = new Date()
-      var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-      var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
-      const registeredDateTime = date+time;
-      userSchema.create({...req.body, registeredDateTime: registeredDateTime}, (error, data) => {
+      userSchema.find({ userName: userName }, (error, data) => {
         if (error) {
-          res.json({ success: false })
+          res.json({ success: false, message: "There was an error..." })
+        }
+        else if (data[0]) {
+          console.log("Already existing user name...")
+          res.json({ success: false, message: "Already existing user name..." })
         } else {
-          console.log("Added a new user")
-          res.json({ success: true, data: data })
+          var today = new Date()
+          var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+          var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+          const registeredDateTime = date + time;
+          userSchema.create({ ...req.body, registeredDateTime: registeredDateTime }, (error, data) => {
+            if (error) {
+              res.json({ success: false })
+            } else {
+              console.log("Added a new user")
+              res.json({ success: true, data: data })
+            }
+          })
         }
       })
     }
