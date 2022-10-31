b8f06e42b1603e1879877eeead1ec1979c52ca9c
LovelyDev829 <lovelydeveloper829@gmail.com>
2022-11-01 01:44:26 +0900

    update a frame

--- routes/frame.route.js
+++ routes/frame.route.js
@@ -44,6 +44,37 @@ router.route('/all-frames').get(async (req, res) => {
     return res.status(200).json(tempFrames)
 })
 
+// CREATE 
+router.route('/update-frame').post((req, res, next) => {
+    const { frameId, frame } = req.body
+    console.log("update-frame", req.body)
+    frameSchema.find({
+        frame: frame
+    }, (error, data) => {
+        if (error) {
+            res.json({ success: false })
+        }
+        else if (data[0]) {
+            // console.log(data[0])
+            console.log("Already existing pattern")
+            res.json({ success: false })
+        } else {
+            var today = new Date()
+            var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+            var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+            const commentDateTime = date + ' / ' + time;
+            frameSchema.findByIdAndUpdate(frameId, {$set: {...req.body, commentDateTime:commentDateTime }}, (error, data) => {
+                if (error) {
+                    res.json({ success: false })
+                } else {
+                    console.log("Added a new pattern..")
+                    res.json({ success: true })
+                }
+            })
+        }
+    })
+})
+
 router.route('/delete-frame/:id').delete((req, res, next) => {
     frameSchema.findByIdAndRemove(req.params.id, (error, data) => {
         if (error) {
